530495535aba56d848a877a917bd2f99e91f922d
ronadniho <463329429@qq.com>
2018-07-13 10:40:04 +0800

    first

---
+++ config/config.js
@@ -0,0 +1,7 @@
+exports.config =  {
+  "testUrl": "https://test.e-celap.com",
+  //"ZWYproductUrl":"https://test.e-celap.com/appzwy",//打包测试环境
+  "ZWYproductUrl":"https://cela.e-celap.com",
+  "imgUrl":"https://file.e-celap.com/FileDownload"
+
+}
